e044acd3419eac722346feb9d1f2009955ce162b
Jacob Greenwald <jeg4t@virginia.edu>
2018-09-14 22:55:22 -0400

    working search!!

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,7 +1,53 @@
 <template>
     <div class="search">
-        <h1>{{$getSpaceSearch()}}</h1>
+        
+        <search-form :makeSearch="handleSearch"/>
+
+        <div v-for="(result, index) in results" :key="index">
+            <h1>{{result.title}}</h1>
+            <p>{{result.description}}</p>
+            <img :src="result.href" :alt="result.title">
+        </div>
+
     </div>
 </template>
 
 
+<script>
+
+import SearchForm from '@/components/SearchForm.vue'
+
+export default {
+
+    // name of component, this is the parent component
+    name: 'search',
+
+    // alias component to kebob case because conventions
+    components: {
+        'search-form' : SearchForm
+    },
+
+    data() {
+        return {
+
+            // final result
+            results: [],
+        }
+    },
+
+    methods: {
+
+      // handle search method will be passed down to child component search form
+      // passing in input as argument  
+      handleSearch ( searchTerm ) {
+
+        // get results of api call using plugin instance variable defined in services/spaceSearch.js   
+        let searchResults = this.$getSpaceSearch( searchTerm )
+          
+          // set results to array so that it can be displayed in component
+          this.results = searchResults
+      
+        }
+    }
+}
+</script>

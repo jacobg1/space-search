e7f7702f9716e4b5a3c54da529b6c10791f7b694
Jacob Greenwald <jeg4t@virginia.edu>
2018-09-14 22:54:06 -0400

    create search form child component

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="search-form">
+
+        <form @submit.prevent>
+            <input type="text" v-model="searchTerm">
+            <button type="submit" @click="makeSearch(searchTerm)">search</button>
+        </form>
+
+    </div>
+</template>
+
+
+<script>
+
+export default {
+
+    // name of component, this is the child component
+    name: 'SearchForm',
+
+    // need this in order to pass down a function as props
+    props: {
+        makeSearch: Function
+    },
+
+    data() {
+
+        // create variable to hold the search term
+        // this will be passed up to parent component in makeSearch callback
+        return {
+            searchTerm: ''
+        }
+    }
+}
+</script>

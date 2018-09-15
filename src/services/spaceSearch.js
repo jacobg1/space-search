310f5d638a2105ba978c75c628c99cd7e4131c9f
Jacob Greenwald <jeg4t@virginia.edu>
2018-09-14 22:54:31 -0400

    finish api search service

--- src/services/spaceSearch.js
+++ src/services/spaceSearch.js
@@ -1,12 +1,33 @@
-// import axios from 'axios'
+import axios from 'axios'
 
 // create search plugin object
 const spaceSearch = {}
+
 spaceSearch.install = function (Vue, options) {
+  
   // create search instance
-  Vue.prototype.$getSpaceSearch = function () {
-    let xxx = 'search!'
-    return xxx
+  Vue.prototype.$getSpaceSearch = function (term) {
+
+    // create empty array that will hold search results
+    var dataArray = []
+
+    // make get request with axios, passing in search term
+    axios({
+      method: 'GET',
+      url: `http://localhost:3001/search/${term}`,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(function (response) {
+
+        // loop through result and push each item to the dataArray
+        response.data.forEach(element => {
+          dataArray.push(element)
+        })
+      })
+      
+    return dataArray
   }
 }
 

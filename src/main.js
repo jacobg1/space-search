7ac01a6d277cd70fd58d441fc0edf387a63a464f
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-15 23:33:12 -0400

    add vue masonry css plugin

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import spaceSearch from './services/spaceSearch'
+import VueMasonry from 'vue-masonry-css'
 
 Vue.use(spaceSearch)
+Vue.use(VueMasonry)
 Vue.config.productionTip = true
 
 new Vue({

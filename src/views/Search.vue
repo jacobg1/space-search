33659eca06fa2230ba706b8487ca9285e595e5f3
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-16 00:20:04 -0400

    configure waterfall

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -3,26 +3,24 @@
         
         <search-form :makeSearch="handleSearch"/>
 
-        <!-- <div v-masonry transition-duration="0.3s" id="gutter-item" item-selector=".item"> -->
-            <masonry
-                :cols="{default: 3, 1000: 2, 700: 1}"
-            >
-            <!-- <h1>{{ result.title }}</h1> -->
+        <Waterfall :resizable=true>
             
-            <!-- <p>{{ result.description_508 }}</p> -->
-
-            <!-- <p class="keywords" v-for="(keyword, index) in result.keywords" :key="index">{{ keyword }}</p> -->
-            <div class="item" v-for="(result, index) in results" :key="index">
-                <img  :src="result.href" :alt="result.title">
-                <!-- <h1>{{ result.title }}</h1> -->
+          <WaterfallItem :width=300 v-for="(result, index) in results" :key="index">
             
-            <!-- <p>{{ result.description_508 }}</p> -->
+            <img :src="result.href" :alt="result.title">
+            
+            <h2>{{ result.title }}</h2>
+            
+            <p>{{ result.description_508 }}</p>
 
-            <!-- <p class="keywords" v-for="(keyword, index) in result.keywords" :key="index">{{ keyword }}</p> -->
+            <div class="keywords" v-for="(keyword, index) in result.keywords" :key="index">
+                <span>{{ keyword }}</span>
             </div>
-       </masonry>
-       <!-- </div> -->
-
+          
+          </WaterfallItem>
+          
+        </Waterfall>
+    
     </div>
 </template>
 
@@ -30,6 +28,8 @@
 <script>
 
 import SearchForm from '@/components/SearchForm.vue'
+import { Waterfall, WaterfallItem } from 'vue2-waterfall'
+
 export default {
 
     // name of component, this is the parent component
@@ -37,7 +37,11 @@ export default {
 
     // alias component to kebob case because conventions
     components: {
-        'search-form' : SearchForm
+        'search-form' : SearchForm,
+
+        // pull in waterfall components
+        Waterfall,
+        WaterfallItem
     },
 
     data() {
@@ -74,11 +78,10 @@ export default {
   
 // }
 .search {
-    max-width: 1300px;
-    margin: 0 auto;
+   
 }
 img {
-    max-width: 425px;
+    max-width: 291px;
   }
 // #test {
 //     width: 200px;

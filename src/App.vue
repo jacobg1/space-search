3c7bf4cafa19e2fb576f5438a135397f62c32039
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-22 19:24:02 -0400

    configure particles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
 <template>
   <div id="app">
+    <vue-particles 
+       color="#ffffff"
+        :particleOpacity="1"
+        :particlesNumber="355"
+        shapeType="circle"
+        :particleSize="2"
+        linesColor="#17182f"
+        :linesWidth="1"
+        :lineLinked="false"
+        :lineOpacity="0"
+        :linesDistance="150"
+        :moveSpeed=".5"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+    >
+    </vue-particles>
+
+
     <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
@@ -16,6 +36,31 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+body,
+html {
+    height: 100%
+}
+
+#particles-js canvas {
+    display: block;
+    vertical-align: bottom;
+    -webkit-transform: scale(1);
+    -ms-transform: scale(1);
+    transform: scale(1);
+    opacity: 1;
+    -webkit-transition: opacity .8s ease, -webkit-transform 1.4s ease;
+    transition: opacity .8s ease, transform 1.4s ease
+}
+
+#particles-js {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    z-index: -10;
+    top: 0;
+    left: 0;
+    background-color: #17182f;
 }
 #nav {
   padding: 30px;

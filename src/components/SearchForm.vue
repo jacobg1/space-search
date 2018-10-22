dbb802f2b2a76e52a400db89f8389b51b5b7f5af
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-22 19:30:40 -0400

    styling

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,9 +1,10 @@
 <template>
+
     <div class="search-form">
 
         <form @submit.prevent>
-            <input type="text" v-model="searchTerm">
-            <button type="submit" @click="makeSearch(searchTerm)">search</button>
+            <input type="text" v-model="searchTerm" placeholder="Enter search">
+            <button class="shutter-button" type="submit" @click="makeSearch(searchTerm)">Go!</button>
         </form>
 
     </div>
@@ -32,3 +33,80 @@ export default {
     }
 }
 </script>
+
+<style scoped lang="scss">
+
+.search-form {
+
+    label, input, button {
+        font-size: inherit;
+        padding: 12px;
+        margin: 0.1em 0.3em;
+        -moz-box-sizing: content-box; 
+        -webkit-box-sizing: content-box;
+        box-sizing: content-box;
+    }
+
+    button {
+        border: 1px solid #ffffff;
+        background-color: #17182f;
+        color: #ffffff;
+        cursor: pointer;
+    }
+    .shutter-button {
+        display: inline-block;
+        -webkit-transform: perspective(1px) translateZ(0);
+        transform: perspective(1px) translateZ(0);
+        box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+        position: relative;
+        background: #17182f;
+        -webkit-transition-property: color;
+        transition-property: color;
+        -webkit-transition-duration: 0.3s;
+        transition-duration: 0.3s;
+    }
+    .shutter-button:before {
+        content: "";
+        position: absolute;
+        z-index: -1;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background: #c2c2de;
+        -webkit-transform: scaleY(0);
+        transform: scaleY(0);
+        -webkit-transform-origin: 50%;
+        transform-origin: 50%;
+        -webkit-transition-property: transform;
+        transition-property: transform;
+        -webkit-transition-duration: 0.3s;
+        transition-duration: 0.3s;
+        -webkit-transition-timing-function: ease-out;
+        transition-timing-function: ease-out;
+    }
+    .shutter-button:hover, 
+    .shutter-button:active {
+        color: #17182f;
+    }
+    .shutter-button:hover:before, 
+    .shutter-button:active:before {
+        -webkit-transform: scaleY(1);
+        transform: scaleY(1);
+    }
+    input:focus, 
+    .shutter-button:focus {
+        outline-color: #f8dbff;
+    }
+    input {
+        width: 191px;
+        background-color: #17182f;
+        border: 1px solid #ffffff;
+        color: #ffffff;
+        font-size: 16px;
+        &::placeholder {
+            color: #ffffff;
+        }
+      }
+  }
+</style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,27 +1,30 @@
 <template>
+
     <div class="search">
-        
-        <search-form :makeSearch="handleSearch"/>
+
+      <h2>Space Search</h2>
+
+      <search-form :makeSearch="handleSearch"/>
 
         <Waterfall :gutterHeight=10 :gutterWidth=10 :resizable=true>
-            
+
           <WaterfallItem :width=300 v-for="(result, index) in results" :key="index">
-            
+
             <h2>{{ result.title }}</h2>
 
             <img :src="result.href" :alt="result.title">
-                        
+
             <p>{{ result.description_508 }}</p>
 
             <!-- <div class="keywords" v-for="(keyword, index) in result.keywords" :key="index">
                 <span>{{ keyword }}</span>
             </div> -->
-          
-          </WaterfallItem>
-          
+          </WaterfallItem>          
+
         </Waterfall>
-    
+
     </div>
+
 </template>
 
 
@@ -74,24 +77,43 @@ export default {
 
 <style scoped lang="scss">
   .search {
-      background: #83add2;
+      /* background: #83add2; */
+      h2 {
+          color: #c2c2de;
+          font-size: 40px;
+      }
+  }
+  .search-form {
+      input {
+          width: 191px;
+          background-color: none;
+      }
   }
   .waterfall-item {
-      background: #d3d3da;
-       box-shadow: 0 1px 3px rgba(0, 0, 0, 0.3);
+      /* background: #d3d3da; */
+       /* box-shadow: 0 1px 3px rgba(0, 0, 0, 0.3); */
+       /* box-shadow: 0px 0px 3px 0px rgba(169, 169, 169, 0.98); */
+       box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+        /* box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23); */
       img {
         max-width: 265px;
+    }
+    h2, p {
+        color: #c2c2de;
     }
     h2 {
         margin: 20px auto;
         padding-top: 20px;
         max-width: 287px;
+        font-size: 18px;
     }
     p {
         padding-bottom: 20px;
         padding-bottom: 20px;
-    max-width: 280px;
-    margin: 20px auto;
+        max-width: 280px;
+        margin: 20px auto;
+        font-size: 15px;
+        font-weight: bold;
     }
   }
 </style>

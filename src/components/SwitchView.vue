64337e73e3f76e3b652062b1022362d9216af721
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-23 20:59:46 -0400

    add switch view component

---
+++ src/components/SwitchView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="switch-view">
+      <button v-bind:class="{ active: listActive }" class="shutter-button" @click="makeSwitch(true)">List</button>
+      <button v-bind:class="{ active: !listActive }" class="shutter-button" @click="makeSwitch(false)">Grid</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SwitchView',
+  props: {
+    // passed down from Search component
+    makeSwitch: Function,
+
+    // gives corresponding button active state
+    listActive: Boolean
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>

b591561bb167ebef629cf3331e7df2f91daedbeb
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-28 16:34:11 -0400

    change list button color

--- src/components/SwitchView.vue
+++ src/components/SwitchView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="switch-view">
-      <button v-bind:class="{ active: listActive }" class="shutter-button" @click="makeSwitch(true)">List</button>
+      <button v-bind:class="{ active: listActive }" class="shutter-button new-background" @click="makeSwitch(true)">List</button>
       <button v-bind:class="{ active: !listActive }" class="shutter-button" @click="makeSwitch(false)">Grid</button>
   </div>
 </template>

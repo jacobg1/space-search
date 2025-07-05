7843aabc24049d2348d34e539820de6a607b434d
jacobg1 <greenwald.j8@gmail.com>
2025-07-05 08:53:19 -0400

    move variable to env file

--- src/services/spaceSearch.ts
+++ src/services/spaceSearch.ts
@@ -1,11 +1,6 @@
 import type { SearchResponse } from '@/types/search'
 import axios from 'axios'
 
-// export global event store
-// export const GlobalEventStore = new Vue()
-
-const API_URL = 'https://03pzs8yfhl.execute-api.us-east-1.amazonaws.com/prod/search'
-
 export default {
   install: (app: any) => {
     const getSpaceSearch = async (term: string, page: string): Promise<SearchResponse> => {
@@ -13,7 +8,7 @@ export default {
       try {
         const response = await axios<SearchResponse>({
           method: 'GET',
-          url: `${API_URL}/${term}${pagination}`,
+          url: `${import.meta.env.VITE_API_URL}/${term}${pagination}`,
           headers: {
             'Content-Type': 'application/json'
           }
